14aa9ea72a6b1409201b4b71c81ab3ea74946246
Fabio Ginzel <fabio.ginzel@pandora.com.br>
2015-09-17 01:15:11 -0300

    Codigo da ocorrência para banco com menos de 3 caracteres

--- src/Cnab/Retorno/CodigoOcorrencia.php
+++ src/Cnab/Retorno/CodigoOcorrencia.php
@@ -14,6 +14,8 @@ class CodigoOcorrencia
         $codigo_ocorrencia  = (int)$codigo_ocorrencia;
         $yamlLoad           = new \Cnab\Format\YamlLoad($codigo_banco);
         $array              = $yamlLoad->loadFormat($format, 'retorno/codigo_ocorrencia');
+        
+        $codigo_banco       = str_pad($codigo_banco,3,'0',STR_PAD_LEFT);
 
         if(array_key_exists($codigo_banco, $array) && array_key_exists($codigo_ocorrencia, $array[$codigo_banco]))
         {
